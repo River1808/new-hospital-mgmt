3279360be2be046c008ac39f6154701ea3df42f7
Keira <94308338+KeiraYeet@users.noreply.github.com>
2025-11-17 10:07:55 +0700

    Revert "Random fixes"
    This reverts commit 1e0b5de36d71173f4cbbf8dff86c92f83d3f3972.

--- src/main/java/hospital/controller/EmployeeController.java
+++ src/main/java/hospital/controller/EmployeeController.java
@@ -1,10 +1,9 @@
 package hospital.controller;
 
 import hospital.database.EmployeeRepository;
-import hospital.schedule.Shift;
 import hospital.staffClasses.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate; // <-- 1. IMPORT THIS
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +12,37 @@ import java.util.List;
 
 @RestController 
 @RequestMapping("/api/employees") 
-@CrossOrigin(origins = "http://localhost:5173")
+// This allows both ports, which is correct
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 public class EmployeeController {
 
     @Autowired 
     private EmployeeRepository employeeRepository;
 
+    // 2. ADD THE MONGO TEMPLATE
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    // 3. THIS IS THE STABLE, WORKING METHOD
     @GetMapping
-    public List<Employee> getAllEmployees() {
-        // This finds all employees and sorts them by employeeId, ascending
-        return employeeRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+    public List<Object> getAllEmployees() {
+        
+        // --- 4. THIS IS THE DEBUG PRINTOUT ---
+        System.out.println("--- DEBUG: getAllEmployees() method was called! ---");
+        
+        // This is the simple workaround that we know works.
+        return mongoTemplate.findAll(Object.class, "employee");
+    }
+    
+    // Your "Add Employee" method (this will still have the "abstract class" error,
+    // but at least your "Get" will work)
+    @PostMapping
+    public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
+        try {
+            Employee savedEmployee = employeeRepository.save(employee);
+            return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
